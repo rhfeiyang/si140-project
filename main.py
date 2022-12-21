bbb555085037843c3828cd12f7644fad6d2549c5
rhfeiyang <rhfeiyang@qq.com>
2022-12-21 11:36:54 +0800

    bug fix， using max for cluster

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import copy
 # super parameters
 import numpy as np
 
-actual_theta = [0.7,0.7,0.5]
+actual_theta = [0.6,0.7,0.5]
 N = 5000
 trial_times = 200
 arms_part1 = [1, 2, 3]
@@ -20,6 +20,8 @@ TS_ab = [[[1, 1], [1, 1], [1, 1]],
 
 def reward_part1(choose):
     # choose: 1,2,3
+    if(choose!=1 and choose !=2 and choose!=3):
+        print(choose,"wrong")
     probability = actual_theta[choose - 1]
     if (random.uniform(0, 1) < probability):
         return 1
@@ -160,15 +162,16 @@ def depend_UCB(N, arms, c):
     for i, cluster in enumerate(cluster_set):
         cluster_count[i]+=len(cluster)
         # cluster_count[i] += 1
-        cluster_theta[i]+=sum([arm_theta[arm-1] for arm in cluster])/cluster_count[i]
+        # cluster_theta[i]+=sum([arm_theta[arm-1] for arm in cluster])/cluster_count[i]
+        cluster_theta[i] = max([arm_theta[arm - 1] for arm in cluster])
 
     for t in range(1,N+1):
         # select cluster
-        cluster_choose = np.argmax(cluster_theta + c * np.sqrt(2 * math.log(t) / cluster_count))
-
+        # cluster_choose = np.argmax(cluster_theta + c * np.sqrt(2 * math.log(t) / cluster_count))
+        cluster_choose = np.argmax(cluster_theta)
         # select and pull arm
-        arm_choose = 1
-        arg_max = 0
+        arm_choose = -1
+        arg_max = -1
         for j in cluster_set[cluster_choose]:
             arg = arm_theta[j-1] + c * math.sqrt(2 * math.log(t) / arm_count[j-1])
             if arg > arg_max:
@@ -177,13 +180,17 @@ def depend_UCB(N, arms, c):
         #print('cluster:', cluster_theta)
         # print(arm_choose)
         # update distribution
-        r = reward_part1(arm_choose)
+        r = reward_part1(arm_choose+1)
         arm_count[arm_choose] += 1
         arm_theta[arm_choose] += 1 / (arm_count[arm_choose]) * (r - arm_theta[arm_choose])
         cluster_count[cluster_choose] += 1
-        cluster_theta[cluster_choose] += 1 / (cluster_count[cluster_choose]) * (r - cluster_theta[cluster_choose])
+        # cluster_theta[cluster_choose] += 1 / (cluster_count[cluster_choose]) * (r - cluster_theta[cluster_choose])
+
+        for i, cluster in enumerate(cluster_set):
+            cluster_theta[i] = max([arm_theta[arm - 1] for arm in cluster])
 
         total_reward += r
+
     # compute the expectation!!
     # result = []
     # for j in arms:
