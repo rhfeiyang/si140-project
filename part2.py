86dd516888bfe2ed6b9958ea162b0421ef6dccf4
WanchenSu <87971041+WanchenSu@users.noreply.github.com>
2023-01-12 17:51:06 +0800

    Add files via upload

--- part2.py
+++ part2.py
@@ -14,7 +14,6 @@ R_choose=[[[[-1 for j4 in range(test_round)] for j3 in range(test_round)] for j2
 
 def reward_part2(choose,time):
     # choose: 1,2
-    assert choose>=0
     probability = actual_theta[choose]
     if (random.uniform(0, 1) < probability):
         return gamma**time
@@ -43,6 +42,40 @@ def R1(a1, b1, a2, b2):
 def R2(a1, b1, a2, b2):
     c=a2/(a2+b2)
     return c*(1+gamma*R(a1,b1,a2+1,b2))+(1-c)*(gamma*R(a1,b1,a2,b2+1))
+
+# def R1(a1, b1, a2, b2):
+#     if EstimatedReward1[a1][b1] != 0:
+#         return EstimatedReward1[a1][b1]
+#     if a1==test_round-1 and b1==test_round-1:
+#         EstimatedReward1[a1][b1] = 1/2
+#     elif a1==test_round-1:
+#         r = R(a1, b1 + 1, a2, b2)
+#         EstimatedReward1[a1][b1] = a1/(a1+b1) + (b1*gamma*r)/(a1+b1)
+#     elif b1==test_round-1:
+#         r = R(a1 + 1, b1, a2, b2)
+#         EstimatedReward1[a1][b1] = (a1*(1+gamma*r))/(a1+b1)
+#     else:
+#         r1 = R(a1, b1 + 1, a2, b2)
+#         r2 = R(a1 + 1, b1, a2, b2)
+#         EstimatedReward1[a1][b1] = (a1*(1+gamma*r2))/(a1+b1) + (b1*gamma*r1)/(a1+b1)
+#     return EstimatedReward1[a1][b1]
+#
+# def R2(a1, b1, a2, b2):
+#     if EstimatedReward1[a2][b2] != 0:
+#         return EstimatedReward1[a2][b2]
+#     if a2==test_round-1 and b2==test_round-1:
+#         EstimatedReward2[a2][b2] = 1/2
+#     elif a2==test_round-1:
+#         r = R(a1, b1, a2, b2 + 1)
+#         EstimatedReward2[a2][b2] = a2/(a2+b2) + (b2*gamma*r)/(a2+b2)
+#     elif b2==test_round-1:
+#         r = R(a1, b1, a2 + 1, b2)
+#         EstimatedReward2[a2][b2] = (a2*(1+gamma*r))/(a2+b2)
+#     else:
+#         r1 = R(a1, b1, a2, b2 + 1)
+#         r2 = R(a1, b1, a2 + 1, b2)
+#         EstimatedReward2[a2][b2] = (a2*(1+gamma*r2))/(a2+b2) + (b2*gamma*r1)/(a2+b2)
+#     return EstimatedReward1[a2][b2]
 
 
 
@@ -84,8 +117,6 @@ def R_part2(N):
     a = [1,1]
     b = [1,1]
     for n in range(N):
-        if R_choose[a[0]][b[0]][a[1]][b[1]]==-1:
-            R(a[0],b[0],a[1],b[1])
         arm_choose = R_choose[a[0]][b[0]][a[1]][b[1]]
         r = reward_part2(arm_choose-1,n)
         if(r != 0):
@@ -95,7 +126,7 @@ def R_part2(N):
         total_reward += r
     return total_reward
 
-# print(R(1,1,1,1))
+print(R(1,1,1,1))
 result1 = 0
 result2 = 0
 for n in range(100):
