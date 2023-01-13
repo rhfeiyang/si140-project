52d7e512e4733717d34c4a124129ad7e85a84668
rhfeiyang <rhfeiyang@qq.com>
2023-01-13 15:15:52 +0800

    part2

--- part2.py
+++ part2.py
@@ -1,12 +1,12 @@
 import random
 import numpy as np
-
+import cython.parallel as parallel
 gamma=0.9
-actual_theta = [0.7, 0.5]
-N = 25
+actual_theta = [0.6,0.9]
+N = 50
 arms = [1,2]
 
-test_round = 40
+test_round = 60
 # EstimatedReward1 = [[0 for i in range(test_round)] for j in range(test_round)]
 # EstimatedReward2 = [[0 for i in range(test_round)] for j in range(test_round)]
 R_result=[[[[-1 for j4 in range(test_round)] for j3 in range(test_round)] for j2 in range(test_round)] for j1 in range(test_round)]
@@ -14,6 +14,7 @@ R_choose=[[[[-1 for j4 in range(test_round)] for j3 in range(test_round)] for j2
 
 def reward_part2(choose,time):
     # choose: 1,2
+    assert choose>=0
     probability = actual_theta[choose]
     if (random.uniform(0, 1) < probability):
         return gamma**time
@@ -26,14 +27,18 @@ def R(a1, b1, a2, b2):
     if R_result[a1][b1][a2][b2]!=-1:
         return R_result[a1][b1][a2][b2]
     else:
-        r = [R1(a1,b1,a2,b2),R2(a1,b1,a2,b2)]
-        R=max(r[0],r[1])
-        R_result[a1][b1][a2][b2]=R
-        if(r[0]>r[1]):
-            R_choose[a1][b1][a2][b2] = 1
+        r = np.array([R1(a1,b1,a2,b2),R2(a1,b1,a2,b2)])
+        # print(r)
+        if r[0]==r[1]:
+            R_choose[a1][b1][a2][b2]=0
         else:
-            R_choose[a1][b1][a2][b2] = 2
-        return R
+            R_choose[a1][b1][a2][b2]=np.argmax(r)+1
+            # print(np.argmax(r)+1)
+        # if a1<3 and a2<3 and b1<3 and b2<3:
+        #     print(a1,b1,a2,b2,r,"choose",str(R_choose[a1][b1][a2][b2]))
+        R_result[a1][b1][a2][b2]=max(r)
+        # print(a1,b1,a2,b2,R_result[a1][b1][a2][b2])
+        return R_result[a1][b1][a2][b2]
 
 def R1(a1, b1, a2, b2):
     c=a1/(a1+b1)
@@ -116,8 +121,20 @@ def R_part2(N):
     total_reward = 0.0
     a = [1,1]
     b = [1,1]
+
+    # for n in range(2):
+    #     r = reward_part2(n, n)
+    #     if (r != 0):
+    #         a[n] += 1
+    #     else:
+    #         b[n] += 1
+    #     total_reward += r
+
     for n in range(N):
         arm_choose = R_choose[a[0]][b[0]][a[1]][b[1]]
+        if arm_choose==0:
+            arm_choose=random.randint(1,2)
+        # print(arm_choose)
         r = reward_part2(arm_choose-1,n)
         if(r != 0):
             a[arm_choose-1] += 1
@@ -126,11 +143,19 @@ def R_part2(N):
         total_reward += r
     return total_reward
 
-print(R(1,1,1,1))
+# print(R(1,1,1,1))
+# print(R1(1,1,1,1),R2(1,1,1,1))
+R(1,1,1,1)
 result1 = 0
 result2 = 0
-for n in range(100):
+times=20000
+
+for n in parallel.prange(times):
     result1 += Part2(N,arms)
-    result2 += R_part2(N)
-print(result1/100)
-print(result2/100)
+
+    r2=R_part2(N)
+    result2 += r2
+    # print(r2)
+print("actual theta",actual_theta)
+print(result1/times)
+print(result2/times)
